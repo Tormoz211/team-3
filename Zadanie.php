c6db0bf358e154c21020978331e59aff5c2ca6d2
Vladimir Sergeev <frostt315@gmail.com>
2022-09-23 21:38:38 +0500

    sergeev

--- Zadanie.php
+++ Zadanie.php
@@ -5,8 +5,25 @@ for($i=1; $i <=30; $i++){
 }
 
 
+$i=1;
+for($i==1;$i<=50;$i++){
+   
 
-for($i=1; $i <=50; $i++){
-    if ($i% 3 == 0 && $i%5 == 0){
-        echo $i, '<br>';}
+    if($i % 3 == 0) {
+        echo "<div class='element50 kursiv'>".$i." номер элемента</div>";
+    }else{
+        echo "<div class='element50'>".$i." номер элемента</div>";
+    }
 }
+?>
+<style>
+    .element50{
+        background-color: #1486ff;
+        padding: 10px;
+        margin: 5px;
+        display: inline-block;
+    }
+    .kursiv{
+        font-style: italic;
+    }
+</style>
